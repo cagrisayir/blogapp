6b6b3e5ef813d1e0c4581ac6affb4b79d2d2b7b3
Omer Cagri Sayir <omercagris@gmail.com>
2024-03-05 11:32:54 +0300

    refactor - constant values

--- src/main/java/com/cagrisayir/blogapp/controller/PostController.java
+++ src/main/java/com/cagrisayir/blogapp/controller/PostController.java
@@ -3,6 +3,7 @@ package com.cagrisayir.blogapp.controller;
 import com.cagrisayir.blogapp.payload.PostDto;
 import com.cagrisayir.blogapp.payload.PostResponse;
 import com.cagrisayir.blogapp.service.PostService;
+import com.cagrisayir.blogapp.utils.AppConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,10 +28,10 @@ public class PostController {
 
     @GetMapping("")
     public PostResponse getAllPosts(
-            @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
-            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
-            @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy,
-            @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
     ) {
         return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
